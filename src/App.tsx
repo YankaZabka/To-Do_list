c5945af4a5b291bdec187177f3a7b21492b743f4
YankaZabka <yanka.zabka@gmail.com>
2021-12-13 17:53:57 +0300

    Add AJAX to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./App.module.scss"
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
@@ -12,6 +12,155 @@ function App() {
     const [tasks, setTasks] = useState<Array<IToDo> | undefined>()
     const [completedTasks, setCompletedTasks] = useState<Array<IToDo> | undefined>()
 
+    useEffect(() => {
+        try {
+            fetch('https://jsonplaceholder.typicode.com/todos')
+                .then((response) => response.json())
+                .then((json: Array<IToDo>) => {
+                    setTasks(json.filter(task => !task.completed))
+                    setCompletedTasks(json.filter(task => task.completed))
+                });
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message)
+        }
+
+    }, [])
+
+    const deleteTask = (task: IToDo): void => {
+        try {
+            fetch(`https://jsonplaceholder.typicode.com/todos/${task.id}`, {
+                method: 'DELETE',
+            });
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message)
+        }
+        setTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)
+    }
+
+    const createTask = (): void => {
+        try {
+            fetch(`https://jsonplaceholder.typicode.com/todos`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    userId: 1,
+                    id: Date.now(),
+                    title: inputText,
+                    completed: false,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message)
+        }
+        setInputText("")
+        setTasks(prev => {
+            if (!prev) {
+                return [{
+                    userId: 1,
+                    id: Date.now(),
+                    title: inputText,
+                    completed: false,
+                }]
+            } else return [...prev, {userId: 1, id: Date.now(), title: inputText, completed: false}]
+        })
+    }
+
+    const copyTask = (task: IToDo): void => {
+        try {
+            fetch(`https://jsonplaceholder.typicode.com/todos`, {
+                method: 'POST',
+                body: JSON.stringify({...task, id: Date.now()}),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message)
+        }
+
+        setTasks(prev => prev ? [...prev, {...task, id: Date.now()}] : undefined)
+    }
+
+    const editTask = (): void => {
+        try {
+            fetch(`https://jsonplaceholder.typicode.com/todos/${editedTaskId}`, {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    title: inputText,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message)
+        }
+
+        setInputText("")
+        setTasks(prev => {
+            if (!prev) return
+
+            return prev.map(task => task.id === editedTaskId ? {...task, title: inputText} : task)
+        })
+    }
+
+    const setCompletedToTrue = (task: IToDo): void => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${task.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                completed: !task.completed,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+        setTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)
+        setCompletedTasks(prev => prev ? [...prev, {...task, completed: !task.completed}] : [{
+            ...task,
+            completed: !task.completed
+        }])
+    }
+
+    const setCompletedToFalse = (task: IToDo): void => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${task.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                completed: !task.completed,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+        setCompletedTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)
+        setTasks(prev => prev ? [...prev, {...task, completed: !task.completed}] : [{
+            ...task,
+            completed: !task.completed
+        }])
+    }
+
+    const deleteCompletedTask = (task: IToDo) => {
+        try {
+            fetch(`https://jsonplaceholder.typicode.com/todos/${task.id}`, {
+                method: 'DELETE',
+            });
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message)
+        }
+        setCompletedTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)
+    }
+
     return (
         <div className={classes.App}>
 
@@ -24,33 +173,11 @@ function App() {
                 <div className={classes.ToDo__container}>
 
                     <ToDoList
-                        onAdd={() => {
-                            setInputText("")
-                            setTasks(prev => {
-                                if (!prev) {
-                                    return [{
-                                        userId: 1,
-                                        id: Date.now(),
-                                        title: inputText,
-                                        completed: false,
-                                    }]
-                                } else return [...prev, {userId: 1, id: Date.now(), title: inputText, completed: false}]
-                            })
-                        }}
-                        onCopy={(task) => setTasks(prev => prev ? [...prev, {...task, id: Date.now()}] : undefined)}
-                        onDelete={(task) => setTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)}
-                        onEdit={() => {
-                            setTasks(prev => {
-                                if (!prev) return
-
-                                return prev.map(task => task.id === editedTaskId ? {...task, title: inputText} : task)
-                            })
-                            setInputText("")
-                        }}
-                        onCompletedChange={(task) => {
-                            setTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)
-                            setCompletedTasks(prev => prev ? [...prev, {...task, completed: !task.completed}] : [{...task, completed: !task.completed}])
-                        }}
+                        onAdd={() => createTask()}
+                        onCopy={(task) => copyTask(task)}
+                        onDelete={(task) => deleteTask(task)}
+                        onEdit={() => editTask()}
+                        onCompletedChange={(task) => setCompletedToTrue(task)}
                         onCurrentChange={value => setInputText(value)}
                         currentTask={inputText}
                         onEditedTaskIdChange={value => setEditedTaskId(value)}
@@ -60,11 +187,8 @@ function App() {
 
                     <CompletedList
                         completedTasks={completedTasks}
-                        onDelete={(task) => setCompletedTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)}
-                        onCompletedChange={(task) => {
-                            setCompletedTasks(prev => prev ? prev.filter(item => item.id !== task.id) : undefined)
-                            setTasks(prev => prev ? [...prev, {...task, completed: !task.completed}] : [{...task, completed: !task.completed}])
-                        }}
+                        onDelete={(task) => deleteCompletedTask(task)}
+                        onCompletedChange={(task) => setCompletedToFalse(task)}
                     />
 
                 </div>
